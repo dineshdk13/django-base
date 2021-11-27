796c62dbe818259dec6df30ff92b5bb54c7b6565
Dhinesh <dineshkumar.m@w2ssolutions.com>
2021-11-27 22:07:12 +0530

    static and media path added

--- mybase/settings.py
+++ mybase/settings.py
@@ -26,7 +26,7 @@ TEMPLATE_PATH=os.path.join(BASE_DIR, 'templates/')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+&%=7@41wej@bw&(1rnc*-t-tcg@90kpohq%k@w=^)y6)+@r(5'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -137,12 +137,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATIC_URL = STATIC_ROOT
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, '/static/'),
+    os.path.join(BASE_DIR, 'static'),
 ]
-print(STATIC_ROOT)
+
+"""Do python manage.py collectstatic during deployment to collect static from all
+    apps(inc STATICFILES_DIRS) and store in staticfiles"""
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
